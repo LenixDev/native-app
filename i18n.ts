f43966fec21581418c0e2d8dfe1b56ca4789c644
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-27 15:22:11 +0100

    feat(i18n): add device language detection and async storage for language selection

--- i18n.ts
+++ i18n.ts
@@ -1,15 +1,30 @@
 import { initReactI18next } from 'react-i18next'
 import { use as i18nUse } from 'i18next'
+import { getLocales } from 'expo-localization'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import en from './locales/en.json'
 import ar from './locales/ar.json'
+type Lang = typeof supportedLangs[number]
+const supportedLangs = ['en', 'ar'] as const
+const fallbackLng: Lang = 'en'
+
+const deviceLang = getLocales()[0].languageCode
+// TODO: make fallback to the user input instetad of hardcoded 'en'
+const lng = supportedLangs.find((lang) => lang === deviceLang) ? deviceLang : fallbackLng
+
+const saved = await AsyncStorage.getItem('lang')
 
 i18nUse(initReactI18next).init({
   resources: {
     en: { translation: en },
     ar: { translation: ar },
   },
-  lng: "en",
-  fallbackLng: 'en',
+  lng,
+  fallbackLng,
   interpolation: { escapeValue: false },
 }).catch((err: unknown) => { throw new Error(String(err)) });
+
+export const changeLang = async (lang: Lang) => {
+  await AsyncStorage.setItem('lang', lang)
+}
