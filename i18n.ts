3c6e3e580ee4f76935a1e35ac1622f183183c3a5
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-27 15:51:04 +0100

    refactor(i18n): replace supportedLangs array with Set and define Lang as union type

--- i18n.ts
+++ i18n.ts
@@ -5,13 +5,15 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import en from './locales/en.json'
 import ar from './locales/ar.json'
-type Lang = typeof supportedLangs[number]
-const supportedLangs = ['en', 'ar'] as const
+
+type Lang = 'en' | 'ar'
+const supportedLangs = new Set<Lang>(['en', 'ar'])
 const fallbackLng: Lang = 'en'
 
 const deviceLang = getLocales()[0].languageCode
 // TODO: make fallback to the user input instetad of hardcoded 'en'
-const lng = supportedLangs.find((lang) => lang === deviceLang) ? deviceLang : fallbackLng
+// eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
+const lng = deviceLang !== null && supportedLangs.has(deviceLang as Lang) ? deviceLang : fallbackLng
 
 const saved = await AsyncStorage.getItem('lang')
 
