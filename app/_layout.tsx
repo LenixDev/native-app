0fcde96f660ce982e4348650805157a4d1d9a302
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-27 15:07:26 +0100

    refactor(i18n): simplify init, set default language to en and remove runtime init from layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import '../global.css'
-import { I18nManager } from 'react-native'
 import 'react-native-reanimated'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { Stack } from 'expo-router'
@@ -7,25 +6,10 @@ import { StatusBar } from 'expo-status-bar'
 import { useColorScheme } from '@/hooks/use-color-scheme'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { HeroUINativeProvider } from 'heroui-native'
-import { useEffect } from 'react'
-import { initI18n } from '@/i18n'
-import { useTranslation } from 'react-i18next'
+import '@/i18n'
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
-  const { i18n, ready } = useTranslation()
-
-  useEffect(() => {
-    initI18n()
-      .then(() => {
-        I18nManager.forceRTL(i18n.language === 'ar')
-      })
-      .catch((err: unknown) => {
-        throw new Error(String(err))
-      })
-  }, [])
-
-  if (!ready) return null
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
--- i18n.ts
+++ i18n.ts
@@ -4,14 +4,12 @@ import { use as i18nUse } from 'i18next'
 import en from './locales/en.json'
 import ar from './locales/ar.json'
 
-export const initI18n = async () => {
-  await i18nUse(initReactI18next).init({
-    resources: {
-      en: { translation: en },
-      ar: { translation: ar },
-    },
-    lng: "end",
-    fallbackLng: 'en',
-    interpolation: { escapeValue: false },
-  })
-}
+i18nUse(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    ar: { translation: ar },
+  },
+  lng: "en",
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false },
+}).catch((err: unknown) => { throw new Error(String(err)) });
