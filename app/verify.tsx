e99d60efcbaeb987dc832fd1702f1d77b46ffe9a
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-29 02:23:12 +0100

    feat(verify): integrate OTP verification with toast, navigation and keyboard handling

--- app/verify.tsx
+++ app/verify.tsx
@@ -1,27 +1,43 @@
+import { raise } from "@/lib/utils";
+import { verify } from "@/services/auth";
+import { navigate } from "expo-router/build/global-state/routing";
+import { useLocalSearchParams } from "expo-router/build/hooks";
 import { InputOTP, type InputOTPRef } from "heroui-native/input-otp";
+import { useToast } from "heroui-native/toast";
 import { useRef } from "react";
+import { KeyboardAvoidingView } from "react-native";
 
 export default function Page() {
   const ref = useRef<InputOTPRef>(null);
-  const onComplete = (code: string) => {
-    console.log('OTP completed:', code);
-    setTimeout(() => {
-      ref.current?.clear();
-    }, 1000);
+  const { toast } = useToast()
+  const { phone } = useLocalSearchParams<{ phone: string }>()
+  
+  const onComplete = async (code: string) => {
+    
+    const [success, response] = await verify(phone, code)
+    if (!success) {
+      toast.show(response)
+      ref.current?.clear()
+      return
+    }
+    toast.show('Verified')
+    navigate('/(tabs)/home')
   };
   return (
-    <InputOTP ref={ref} maxLength={6} onComplete={onComplete}>
-      <InputOTP.Group>
-        <InputOTP.Slot index={0} />
-        <InputOTP.Slot index={1} />
-        <InputOTP.Slot index={2} />
-      </InputOTP.Group>
-      <InputOTP.Separator />
-      <InputOTP.Group>
-        <InputOTP.Slot index={3} />
-        <InputOTP.Slot index={4} />
-        <InputOTP.Slot index={5} />
-      </InputOTP.Group>
-    </InputOTP>
+    <KeyboardAvoidingView behavior="padding" className="flex items-center justify-center w-full h-full">
+      <InputOTP ref={ref} maxLength={6} onComplete={self => { onComplete(self).catch(raise) }}>
+        <InputOTP.Group>
+          <InputOTP.Slot index={0} />
+          <InputOTP.Slot index={1} />
+          <InputOTP.Slot index={2} />
+        </InputOTP.Group>
+        <InputOTP.Separator />
+        <InputOTP.Group>
+          <InputOTP.Slot index={3} />
+          <InputOTP.Slot index={4} />
+          <InputOTP.Slot index={5} />
+        </InputOTP.Group>
+      </InputOTP>
+    </KeyboardAvoidingView>
   )
 }
