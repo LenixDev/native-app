00d36422e37afe75acc22d4ff1ad118f87f2b6fe
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-29 01:16:04 +0100

    feat(utils): add flag helper mapping country codes to emoji flags

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,9 @@
+import countries from '@/lib/countries.json' with { type: "json" }
+
 export const raise = (err: unknown) => { throw new Error(String(err)) }
+
+export const flag = countries.map(country => (
+  country.code.toUpperCase().replace(/./g, c =>
+    String.fromCodePoint(127397 + c.charCodeAt(0))
+  )
+))
