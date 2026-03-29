22880135dd445a6897f55c77f4d643874adfc357
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-29 02:22:28 +0100

    feat(auth): add signup and verify functions

--- services/auth.ts
+++ services/auth.ts
@@ -4,3 +4,17 @@ export const signin = async (phone: string, password: string) => {
   const result = await supabase.auth.signInWithPassword({ phone, password })
   return result
 }
+
+export const signup = async (phone: string, password: string) => {
+  const result = await supabase.auth.signUp({ phone, password })
+  return result
+}
+
+export const verify = async (phone: string, token: string): Promise<[true] | [false, string]> => {
+  const { error } = await supabase.auth.verifyOtp({ phone, token, type: 'sms' })
+  if (error) {
+    console.error(phone, token, error)
+    return [false, error.message]
+  }
+  return [true]
+}
