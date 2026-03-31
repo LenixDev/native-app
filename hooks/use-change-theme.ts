eb5e63445df42bdf01154cbecf8d2489a7ed4722
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-31 20:52:31 +0100

    feat(use-change-theme): detect device theme, show i18n error toast, and integrate Uniwind theme setting with added translation keys.

--- hooks/use-change-theme.ts
+++ hooks/use-change-theme.ts
@@ -1,9 +1,15 @@
 import { supabase } from "@/lib/supabase"
 import type { Theme } from "@/types"
 import { useToast } from "heroui-native"
+import { useTranslation } from "react-i18next";
+import { useColorScheme } from "react-native";
+import { Uniwind } from 'uniwind';
 
 export const useChangeTheme = () => {
   const { toast } = useToast()
+  const { t } = useTranslation()
+  const deviceTheme = useColorScheme()
+  if (!deviceTheme) toast.show(t("theme_error"))
 
   return async (theme: Theme) => {
     const { error: userError, data } = await supabase.auth.getUser()
@@ -16,6 +22,7 @@ export const useChangeTheme = () => {
       toast.show(error.message)
       return false
     }
-    return true
+    Uniwind.setTheme(theme === 'system' ? deviceTheme ?? 'light' : theme)
+  return true
   }
 }
