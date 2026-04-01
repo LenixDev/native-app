37bb800947132c15ff3280d0fb2cb0a2f2f90b0e
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-01 01:05:49 +0100

    feat(settings): display user avatar with initials fetched from Supabase

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -2,30 +2,34 @@ import { Appearance } from '@/components/appearance'
 import { BottomModal } from '@/components/bottom-modal'
 import { ListItem } from '@/components/list-item'
 import { useRTL } from '@/hooks/use-rtl'
+import { supabase } from '@/lib/supabase'
 import { raise } from '@/lib/utils'
 import { signout } from '@/services/auth'
 import { router } from 'expo-router'
 import {
+	Avatar,
 	Button,
 	Description,
 	ListGroup,
 	Separator,
 	useToast,
 } from 'heroui-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Text, View } from 'react-native'
 
 // Faq accordittion
+
 // Chat avatart
+
 // Other settings checkbox
 // Controlfield
+// Switch
 // Menu
 // Radio
-// Scroll shadow chat
 
+// Scroll shadow chat
 // Surface chat
-// Switch
 
 // TODO:
 // - Generate memory from chat history to improve response quality
@@ -39,25 +43,54 @@ export default function Tab() {
 	const { toast } = useToast()
 	const [open, setOpen] = useState(false)
 	const rtl = useRTL()
+	const [name, setName] = useState('')
 
 	const handleSignout = () => {
 		signout()
-			.then(([success, response]) => {
-				if (!success) {
-					toast.show(response)
-					return
-				}
-				router.replace('/signin')
-				toast.show(t('signout_success'))
-			})
-			.catch(raise)
+		.then(([success, response]) => {
+			if (!success) {
+				toast.show(response)
+				return
+			}
+			router.replace('/signin')
+			toast.show(t('signout_success'))
+		})
+		.catch(raise)
+	}
+
+	useEffect(() => {
+		supabase.auth.getUser().then(({ error, data }) => {
+			if (error) {
+				toast.show(error.message)
+				return
+			}
+			setName(data.user.user_metadata.display_name)
+		}).catch(raise)
+	})
+
+	if (name.trim() === '') return null
+	
+	const avatarFromName = () => {
+		const words = name.trim().split(/\s+/)
+		if (words.length === 1) return words[0][0]
+		return `${words[0][0]} ${words[1][0]}`
 	}
 
 	return (
 		<View className='flex justify-between h-full p-5'>
-			<View className={`flex-1 py-10 ${rtl('items-end')}`}>
-				<Text className='text-4xl text-foreground'>{t('preferences')}</Text>
-				<Description>{t('manage_preferences')}</Description>
+			<View className={`flex-1 py-10 flex-row${rtl('-reverse')} justify-between`}>
+				<View className={rtl('flex items-end')}>
+					<Text className='text-4xl text-foreground'>{t('preferences')}</Text>
+					<Description>{t('manage_preferences')}</Description>
+				</View>
+				<View>
+					<Avatar color="accent" alt='account-avatar'>
+					<Avatar.Fallback
+						textProps={{ style: { fontSize: 24, lineHeight: 32 } }}
+						styles={{ container: { justifyContent: 'center', alignItems: 'center', paddingBottom: 0, marginBottom: 0 } }}
+					>{avatarFromName()}</Avatar.Fallback>
+					</Avatar>
+				</View>
 			</View>
 
 			<ListGroup className='flex-2 p-0'>
