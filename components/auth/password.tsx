047b5a9407799eaa0a5545a652bd8a3fe0b07c27
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-01 21:53:49 +0100

    feat(auth): add RTL support and extract eye toggle component in password input

--- components/auth/password.tsx
+++ components/auth/password.tsx
@@ -3,19 +3,40 @@ import { t } from 'i18next'
 import { Pressable } from 'react-native'
 import { IconSymbol } from '../ui/icon-symbol'
 import { useState } from 'react'
+import { useIsRTL } from '@/hooks/use-rtl'
+
+const Eye = ({ isVisible, onPress }: { isVisible: boolean; onPress: () => void }) => {
+	const muted = useThemeColor('muted')
+	return (
+		<Pressable
+			hitSlop={20}
+			{...{ onPress }}
+		>
+			<IconSymbol
+				size={16}
+				name={`eye.${isVisible ? 'slash.' : ''}fill`}
+				color={muted}
+			/>
+		</Pressable>
+	)
+}
 
 export const PasswordInput = ({
 	...prop
 }: Record<string, unknown> & { onChangeText: (text: string) => void }) => {
 	const muted = useThemeColor('muted')
 	const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+	const isRtl = useIsRTL()
 
 	return (
 		<InputGroup>
 			<InputGroup.Prefix isDecorative>
-				<IconSymbol color={muted} name='lock.fill' size={16} />
+				{isRtl ? <Eye isVisible={isPasswordVisible} onPress={() => {
+					setIsPasswordVisible(!isPasswordVisible)
+				}} /> : <IconSymbol color={muted} name='lock.fill' size={16} />}
 			</InputGroup.Prefix>
 			<InputGroup.Input
+				textAlign={isRtl ? 'right' : 'left'}
 				returnKeyType='done'
 				placeholder={t('password')}
 				autoCapitalize='none'
@@ -24,18 +45,9 @@ export const PasswordInput = ({
 				{...prop}
 			/>
 			<InputGroup.Suffix>
-				<Pressable
-					hitSlop={20}
-					onPress={() => {
-						setIsPasswordVisible(!isPasswordVisible)
-					}}
-				>
-					<IconSymbol
-						size={16}
-						name={`eye.${isPasswordVisible ? 'slash.' : ''}fill`}
-						color={muted}
-					/>
-				</Pressable>
+				{isRtl ? <IconSymbol color={muted} name='lock.fill' size={16} /> : <Eye isVisible={isPasswordVisible} onPress={() => {
+					setIsPasswordVisible(!isPasswordVisible)
+				}}/>}
 			</InputGroup.Suffix>
 		</InputGroup>
 	)
