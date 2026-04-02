7a9c357077cba084cf7f58945706b294656ba07c
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-02 01:26:10 +0100

    feat(signin): set app language after successful sign‑in

--- app/signin.tsx
+++ app/signin.tsx
@@ -5,13 +5,14 @@ import { useTranslation } from 'react-i18next'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { verificationKey } from '@/constants'
 import { supabase } from '@/lib/supabase'
+import { Lang } from '@/types'
 
 export default function Page() {
-	const { t } = useTranslation()
+	const { t, i18n } = useTranslation()
 	const { toast } = useToast()
 
 	const auth = async (phone: string, password: string) => {
-		const { error } = await supabase.auth.signInWithPassword({ phone, password })
+		const { error, data } = await supabase.auth.signInWithPassword({ phone, password })
 		if (error) {
 			if (error.code === 'phone_not_confirmed') {
 				await AsyncStorage.setItem(verificationKey, phone)
@@ -21,6 +22,12 @@ export default function Page() {
 			toast.show(error.message)
 			return
 		}
+		const { data: account } = await supabase
+			.from('accounts')
+			.select('lang')
+			.eq('id', data.user.id)
+			.single<{ lang: Lang }>()
+		if (account) await i18n.changeLanguage(account.lang)
 		toast.show(t('signin_success'))
 		router.replace('/(tabs)/home')
 	}
